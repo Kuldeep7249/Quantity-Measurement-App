47424585a2fbaf72307bcebcb92df886069b7cb7
Kuldeep7249 <152018228+Kuldeep7249@users.noreply.github.com>
2026-03-17 15:56:10 +0530

    {kuldeep_Singh] Add. Spring integrated Successfully also created the endpoints

--- QuantityMeasurementApp/src/main/java/com/quantitymeasurement/entity/QuantityMeasurementEntity.java
+++ QuantityMeasurementApp/src/main/java/com/quantitymeasurement/entity/QuantityMeasurementEntity.java
@@ -9,7 +9,6 @@ public class QuantityMeasurementEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String operation;
     private double operand1;
     private double operand2;
